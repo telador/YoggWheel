dda56dc3dd9d0ab30d65e35f9b94237ee58623a1
telador <londor13417@gmail.com>
2024-07-12 11:40:29 +0300

    now I simply must learn it

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,9 @@ export default {
         rollupOptions: {
             input: {
                 main: path.resolve(__dirname, "index.html"),
-                wheel: path.resolve(__dirname, "wheel.html"),
-                wheel_viewer: path.resolve(__dirname, "wheel_viewer.html"),
-                popover: path.resolve(__dirname, "popover.html")
+                wheel: path.resolve(__dirname, "src/wheel.html"),
+                wheel_viewer: path.resolve(__dirname, "src/wheel_viewer.html"),
+                popover: path.resolve(__dirname, "src/popover.html")
             }
         }
     }
