8a9740e0ef57b84ba39831537ef47a39a4fb8a75
telador <londor13417@gmail.com>
2024-07-13 16:05:19 +0300

    css evolution

--- src/main.js
+++ src/main.js
@@ -1,67 +1,66 @@
 import OBR from '@owlbear-rodeo/sdk'
 import '/src/style.css'
-
-document.querySelector("#app").innerHTML= `
-<div class="container">
-<button id="switch-btn" class="btn-cr">Create Wheel</button>
-
+let app = document.querySelector("#app");
+app.innerHTML= `
+<div class="app-panel panel">
+    <button class="create-wheel btn-cr" style="min-width: 150px">Create Wheel</button>
+    <input class="name-input" type="text" maxlength="20" placeholder="Wheel Name" />
+    <input class="file-input" type="file" accept=".json" />
+    <button class="upload-button btn-cr">Upload</button>
+    <select class="wheel-select">
+        <option value="">None</option>
+    </select>
+    <button class="open-wheel btn-cr">Open</button>
+    <button class="download-wheel btn-cr">Download</button>
+    <button class="edit-wheel btn-cr">Edit(TBA)</button>
+    <button class="delete-wheel btn-cr">Delete(TBA)</button>
 </div>
-<br>
-<div id="spin-div" class="container">
-    <div>
-        <form id="upload">
-            <input type="text" id="name" maxlenght="20" placeholder="Wheel Name">
-            <input type="file" id="file" accept=".json">
-            <br>
-            <button class="btn-cr">Upload</button>    
-        </form>
-    </div>
-    <br>
-
-    <div>
-        <select id="wheels" class="select">
-            <option value="">None</option>
-        </select>
-    </div>
-    <div>
-        <button id="open-btn" class="btn-cr">Open</button>
-        <button id="save-btn" class="btn-cr">Download</button>
-    </div>
-</div>
-<div id="create-div" class="container" hidden>
-    <input type="text" id="newName" maxlenght="20" placeholder="Wheel Name">
-    <input type="number" id="sector_count" hidden></input>
+<div class="create-panel panel" style="display: none;">
+    <button class="create-wheel btn-cr" style="min-width: 150px">Spin Wheel</button>
+    <input class="name-input" type="text" maxlength="20" placeholder="Wheel Name" />
+    <input type="number" id="sector_count" value=0 hidden></input>
     <input type="number" id="sector_id" value=1 hidden></input>
-    <div id="sector_0" class="container" hidden>
-        <input type="text" id="text" placeholder="text"></input>
-        <input type="text" id="fulltext" placeholder="fulltext"></input>
-        <input type="color" id="color"></input>
-        <select id="reaction" class="select">
+    <div class="sector" style="display:none">
+        <input class="text-input" type="text" placeholder="text"></input>
+        <input class="fulltext-input" type="text" placeholder="fulltext"></input>
+        <input class="color-input" type="color"></input>
+        <select class="reaction-select">
             <option value="resting">Resting</option>
             <option value="dancing">Dancing</option>
             <option value="laughing">Laughing</option>
             <option value="shocked">Shocked</option>
         </select>
-        <button id="del-btn" class="btn-cr">delete</button>
-    </div>
-    <button id="add-btn" class="btn-cr">add</button>
-    <button id="gen-btn" class="btn-cr">Generate</button>
-    <div id="sectors" class="scrollable-div"></div>
+        <button class="delete-sector btn-cr">
+<svg width="20px" height="20px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M18.7069 7.79289C19.0974 8.18342 19.0974 8.81658 18.7069 9.20711L15.914 12L18.7069 14.7929C19.0974 15.1834 19.0974 15.8166 18.7069 16.2071C18.3163 16.5976 17.6832 16.5976 17.2926 16.2071L14.4998 13.4142L11.7069 16.2071C11.3163 16.5976 10.6832 16.5976 10.2926 16.2071C9.90212 15.8166 9.90212 15.1834 10.2926 14.7929L13.0855 12L10.2926 9.20711C9.90212 8.81658 9.90212 8.18342 10.2926 7.79289C10.6832 7.40237 11.3163 7.40237 11.7069 7.79289L14.4998 10.5858L17.2926 7.79289C17.6832 7.40237 18.3163 7.40237 18.7069 7.79289Z" fill="#fff788"></path> <path fill-rule="evenodd" clip-rule="evenodd" d="M6.30958 3.54424C7.06741 2.56989 8.23263 2 9.46699 2H20.9997C21.8359 2 22.6103 2.37473 23.1614 2.99465C23.709 3.61073 23.9997 4.42358 23.9997 5.25V18.75C23.9997 19.5764 23.709 20.3893 23.1614 21.0054C22.6103 21.6253 21.8359 22 20.9997 22H9.46699C8.23263 22 7.06741 21.4301 6.30958 20.4558L0.687897 13.2279C0.126171 12.5057 0.126169 11.4943 0.687897 10.7721L6.30958 3.54424ZM9.46699 4C8.84981 4 8.2672 4.28495 7.88829 4.77212L2.2666 12L7.88829 19.2279C8.2672 19.7151 8.84981 20 9.46699 20H20.9997C21.2244 20 21.4674 19.9006 21.6665 19.6766C21.8691 19.4488 21.9997 19.1171 21.9997 18.75V5.25C21.9997 4.88294 21.8691 4.5512 21.6665 4.32337C21.4674 4.09938 21.2244 4 20.9997 4H9.46699Z" fill="#fff788"></path> </g></svg>
+</div>
+    <button class="add-sector btn-cr">+</button>
+    <button class="generate-wheel btn-cr">Generate</button>
+    <div class="scrollable-div"></div>
 </div>
 `
 
  // @param reaction ['resting' | 'dancing' | 'laughing' | 'shocked'] Sets the reaper's animated reaction
 OBR.onReady(() => {
-    const select = document.getElementById('wheels')
-    const open = document.getElementById('open-btn')
-    const form = document.getElementById('upload')
-    const file = document.getElementById('file')
-    const name = document.getElementById('name')
-    const switch_btn = document.getElementById('switch-btn')
-    const add = document.getElementById('add-btn')
-    const gen = document.getElementById('gen-btn')
-    const save = document.getElementById('save-btn')
-
+    const select = document.getElementsByClassName('wheel-select')[0]
+    const open = document.getElementsByClassName('open-wheel')[0]
+    const form = document.getElementsByClassName('upload-button')[0]
+    const file = document.getElementsByClassName('file-input')[0]
+    const name = document.getElementsByClassName('name-input')[0]
+    const switch_btn = document.getElementsByClassName('create-wheel')
+    const add = document.getElementsByClassName('add-sector')[0]
+    const gen = document.getElementsByClassName('generate-wheel')[0]
+    const save = document.getElementsByClassName('download-wheel')[0]
+    let panels = document.getElementsByClassName('panel')
+    
+    for (let i = 0; i < switch_btn.length; i++){
+        
+        switch_btn[i].addEventListener("click", () => {
+            for (let j = 1; j <= panels.length; j++){
+                panels[j%panels.length].style.display =  ((i == j-1) ? "grid" : "none");
+            }
+            app.style.alignSelf = ((i == 0) ? "normal" : "center");
+        });
+    }
     save.addEventListener("click", () => {
         if (select.value != ''){
             const t = localStorage.getItem(select.value);
@@ -89,19 +88,20 @@ OBR.onReady(() => {
 
     gen.addEventListener("click", () => {
         const counter = document.getElementById('sector_count')
-        const newName = document.getElementById('newName')
-        const sectors = document.getElementById('sectors')
+        const newName = document.getElementsByClassName('name-input')[1]
+        const sectors = document.getElementsByClassName('scrollable-div')[0]
         if(counter.value != 0 && newName.value != ''){
             const k = sectors.children
             let str = '{\n\t"prizes":['
-            for (let t=0;t<k.length;t++){
-                if (k[t].id.slice(0, 7) == "sector_" && k[t].id != "sector_0" && k[t].id != "sector_id" && k[t].id != "sector_count"){
-                    let p = k[t].children
-                    if (str.slice(-1) != '[')
-                        str += ','
-                    str += '\n\t\t{\n\t\t\t"text":"'+p[0].value+'",\n\t\t\t"fulltext":"'+p[1].value+'",\n\t\t\t"color":"'+p[2].value+'",\n\t\t\t"reaction":"'+p[3].value+'"\n\t\t}'
-                }
+                console.log(k)
+            for (let t = 0; t < k.length; t++){
+                let p = k[t].children
+                console.log(p)
+                if (str.slice(-1) != '[')
+                    str += ','
+                str += '\n\t\t{\n\t\t\t"text":"'+p[0].value+'",\n\t\t\t"fulltext":"'+p[1].value+'",\n\t\t\t"color":"'+p[2].value+'",\n\t\t\t"reaction":"'+p[3].value+'"\n\t\t}'
             }
+            
             str += '\n\t]\n}'
             localStorage.setItem(newName.value+"SW", str);
             let el = document.createElement("option")
@@ -111,47 +111,29 @@ OBR.onReady(() => {
             select.appendChild(el)
             select.value = el.value
             save.click()
-            switch_btn.click()
+            switch_btn[1].click()
         }
     });
 
     add.addEventListener("click", () => {
-        let or = document.getElementById('sector_0')
+        let or = document.getElementsByClassName('sector')[0]
         let sector_id = document.getElementById('sector_id')
         let sector_count = document.getElementById('sector_count')
         let el = or.cloneNode(true)
-        el.id = 'sector_'+sector_id.value
-        el.removeAttribute('hidden')
-        el.children['text'].value = ""+sector_id.value
-        el.children['color'].value = getRandomColor()
-        const del_b = el.children['del-btn']
-        del_b.addEventListener("click", (e) => {
+        el.style.display = "grid"
+
+        el.children[0].value = ""+sector_id.value
+        el.children[2].value = getRandomColor()
+        const del_b = el.children[4]
+        del_b.addEventListener("click", () => {
             let sector_count = document.getElementById('sector_count')
-            sector_count--    
-            let parentDiv = e.target.parentElement;
-            parentDiv.remove()
+            sector_count--
+            el.remove()
         });
         sector_id.value++
         sector_count.value++
-        const tar = document.getElementById("sectors")
-        tar.appendChild(el)
-    });
-
-    switch_btn.addEventListener("click", () => {
-        if (switch_btn.textContent == "Create Wheel"){
-            let a = document.getElementById('spin-div')
-            a.setAttribute('hidden', '')
-            let b = document.getElementById('create-div')
-            b.removeAttribute('hidden')
-            switch_btn.textContent = "Spin or Import Wheel"
-        }
-        else {
-            let a = document.getElementById('spin-div')
-            a.removeAttribute('hidden')
-            let b = document.getElementById('create-div')
-            b.setAttribute('hidden', '')
-            switch_btn.textContent = "Create Wheel"
-        }
+        const tar = document.getElementsByClassName("scrollable-div")[0]
+        tar.appendChild(el) 
     });
 
     for (let key in localStorage){
@@ -177,9 +159,6 @@ OBR.onReady(() => {
 
     function logFile (event) {
         let str = event.target.result;
-        let json = JSON.parse(str);
-        //console.log('string', str);
-        //console.log('name', name.value);
         localStorage.setItem(name.value+"SW", str)
         let el = document.createElement("option")
         el.text = name.value
